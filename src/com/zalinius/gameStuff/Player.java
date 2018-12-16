0affc210f3bc0d4bd609606b7d777de76732667a
Zachary Lapointe <Zachary.lapointe@mail.concordia.ca>
2018-12-16 16:11:45 -0500

    Added player colours for halloween and christmas

--- src/com/zalinius/gameStuff/Player.java
+++ src/com/zalinius/gameStuff/Player.java
@@ -6,10 +6,14 @@ import com.zalinius.utilities.ZMath;
 import com.zalinius.physics.Point2D;
 import java.awt.*;
 import java.awt.geom.Ellipse2D;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
 
 public class Player implements GameObject {
 
     private int PLAYER_DIAMETER = 30;
+    private PlayerColor pColor;
 
     public static final double EASY_SHIFT = .25, MEDIUM_SHIFT = .4, HARD_SHIFT = .6;
 
@@ -21,8 +25,9 @@ public class Player implements GameObject {
     private double balance; //Always between -1.0 and 1.0, inclusive
     private double balanceShift;
     private Balance balanceState;
-
-    public Player(Point2D p, double balanceShift){
+    
+    public Player(Point2D p, double balanceShift)
+    {
         this.p = p;
         this.input = new PlayerInput();
         this.alive = true;
@@ -30,6 +35,20 @@ public class Player implements GameObject {
         this.balance = 0.0;
         this.balanceState = Balance.NEUTRAL;
         this.balanceShift = balanceShift;
+        
+        this.pColor = new PlayerColor();
+    }
+
+    public Player(Point2D p, double balanceShift, Color lightColor, Color darkColor){
+        this.p = p;
+        this.input = new PlayerInput();
+        this.alive = true;
+
+        this.balance = 0.0;
+        this.balanceState = Balance.NEUTRAL;
+        this.balanceShift = balanceShift;
+        
+        this.pColor = new PlayerColor(lightColor, darkColor);
     }
 
     public void update(double delta){
@@ -106,25 +125,18 @@ public class Player implements GameObject {
     }
 
     private Color getLightColor(){
-        int lightness;
-        if(balance < 0)
-            lightness = 255 - (int)( balance * -1 * 255);
+        if(balance < 0) {
+        	return pColor.getColour(balance);
+        }
         else
-            lightness = 255;
-
-
-        return new Color(lightness, lightness, lightness);
+        	return pColor.getLightColor();
     }
 
     private Color getDarkColor(){
-        int darkness;
-        if(balance > 0)
-            darkness = (int)( balance * 255);
-        else
-            darkness = 0;
-
-
-        return new Color(darkness, darkness, darkness);
+        if(balance > 0) {
+            return pColor.getColour(balance);
+        }
+        return pColor.getDarkColor();
     }
 
     public void playerLocation(Balance location){
@@ -147,6 +159,68 @@ public class Player implements GameObject {
 
     public boolean isInGoal(){
         return isInGoal;
+    }
+    
+    private class PlayerColor{
+    	private Color light, dark;
+    	
+    	public PlayerColor() {
+    		GregorianCalendar cal = new GregorianCalendar();
+    		int month = cal.get(Calendar.MONTH);
+    		int day = cal.get(Calendar.DAY_OF_MONTH);
+    		System.out.println(month + " " + day);
+    		
+    		if(month == Calendar.DECEMBER && day == 25) {
+    			System.out.println("Christmas!");
+
+        		this.light = new Color(220, 0, 0);
+        		this.dark = new Color(0, 150, 70);
+    		}
+    		else if(month == Calendar.OCTOBER && day == 31) {
+    			System.out.println("Halloween");
+    			this.light = new Color(255, 140, 0);
+    			this.dark = new Color(0, 0, 0);
+    		}
+    		else {
+    			this.light = new Color(255, 255, 255);
+    			this.dark = new Color(0, 0, 0);
+    		}
+
+    	}
+    	
+    	public PlayerColor(Color light, Color dark) {
+    		this.light = light;
+    		this.dark = dark;
+    	}
+    	public Color getLightColor() {
+    		return light;
+    	}
+    	public Color getDarkColor() {
+    		return dark;
+    	}
+    	public Color getMiddle() {
+    		int red = (light.getRed() + dark.getRed())/2;
+    		int green = (light.getGreen() + dark.getGreen())/2;
+    		int blue = (light.getBlue() + dark.getBlue())/2;
+    		
+    		return new Color(red, green, blue);
+    	}
+    	
+    	public Color getColour(double colourBalance) {
+    		double lightMulti, darkMulti;
+    		if(colourBalance > 0) {
+    			lightMulti = Math.abs(colourBalance);
+    			darkMulti = 1 - lightMulti;
+    		}else {
+    			darkMulti = Math.abs(colourBalance);
+    			lightMulti = 1 - darkMulti;
+    		}
+    		int red = (int) (light.getRed()*lightMulti + dark.getRed()*darkMulti);
+    		int green = (int) (light.getGreen()*lightMulti + dark.getGreen()*darkMulti);
+    		int blue = (int) (light.getBlue()*lightMulti + dark.getBlue()*darkMulti);
+    		
+    		return new Color(red, green, blue);
+    	}
     }
 }
 
